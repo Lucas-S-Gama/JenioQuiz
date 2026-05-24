3e93252162360f5300bba773ff20a0f91957c296
Lucas-S-Gama <lucas.gama@sptech.school>
2026-05-24 15:27:29 -0300

    Site JenioFinalizado

---
+++ js/perguntas.js
@@ -0,0 +1,146 @@
+let perguntas = [
+  {
+    titulo: 'Qual desses é o cérebro do computador',
+    alternativas: [
+      { resposta: 'CPU', valor: true },
+      { resposta: 'Memória RAM', valor: false },
+      { resposta: 'Sistema Operacional', valor: false },
+      { resposta: 'BIOS', valor: false }
+    ]
+  },
+  {
+    titulo: 'Quem executa as operações dentro da CPU',
+    alternativas: [
+      { resposta: 'Registradores', valor: false },
+      { resposta: 'ROM', valor: false },
+      { resposta: 'UC', valor: false },
+      { resposta: 'ULA', valor: true }
+    ]
+  },
+  {
+    titulo: 'Quais são as pequenas memórias internas na CPU',
+    alternativas: [
+      { resposta: 'CACHE', valor: false },
+      { resposta: 'UC', valor: false },
+      { resposta: 'Registradores', valor: true },
+      { resposta: 'ULA', valor: false }
+    ]
+  },
+  {
+    titulo: 'Qual memória armazena dados temporariamente',
+    alternativas: [
+      { resposta: 'Memória ROM', valor: false },
+      { resposta: 'Memória FLASH', valor: false },
+      { resposta: 'Memória RAM', valor: true },
+      { resposta: 'Memória de massa', valor: false }
+    ]
+  },
+  {
+    titulo: 'Memória usada em sua maioria em firmware',
+    alternativas: [
+      { resposta: 'Memória EPROM', valor: false },
+      { resposta: 'Memória ROM', valor: true },
+      { resposta: 'Memória FLASH', valor: false },
+      { resposta: 'Memória de massa', valor: false }
+    ]
+  },
+  {
+    titulo: 'Qual memória pode apagar dados com raios ultravioleta',
+    alternativas: [
+      { resposta: 'Memória RAM', valor: false },
+      { resposta: 'Memória EPROM', valor: true },
+      { resposta: 'Memória UC', valor: false },
+      { resposta: 'Memória FLASH', valor: false }
+    ]
+  },
+    {
+    titulo: 'Qual memória é a evolução da EPROM',
+    alternativas: [
+      { resposta: 'Memória FLASH', valor: true },
+      { resposta: 'Memória RAM', valor: false },
+      { resposta: 'Memória ROM', valor: false },
+      { resposta: 'Memória de massa', valor: false }
+    ]
+  },
+  {
+    titulo: 'Qual armazena os dados Permanentemente',
+    alternativas: [
+      { resposta: 'Registradores', valor: false },
+      { resposta: 'Processador', valor: false },
+      { resposta: 'Portal Serial', valor: false },
+      { resposta: 'Memória de massa', valor: true }
+    ]
+  },
+  {
+    titulo: 'Qual recurso da placa-mãe acessa diretamente a memória',
+    alternativas: [
+      { resposta: 'SQL', valor: false },
+      { resposta: 'DMA', valor: true },
+      { resposta: 'JAVA', valor: false },
+      { resposta: 'PAAS', valor: false }
+    ]
+  },
+  {
+    titulo: 'Pino de controle usado em memórias',
+    alternativas: [
+      { resposta: 'UC', valor: false },
+      { resposta: 'IAAS', valor: false },
+      { resposta: 'CS', valor: true },
+      { resposta: 'SAAS', valor: false }
+    ]
+  },
+  {
+    titulo: 'Barramento que é passado o local da memória',
+    alternativas: [
+      { resposta: 'adress bus', valor: false },
+      { resposta: 'addres bus', valor: false },
+      { resposta: 'adres bus', valor: false },
+      { resposta: 'address bus', valor: true }
+    ]
+  },
+  {
+    titulo: 'Barramento que é passado dados e instruções',
+    alternativas: [
+      { resposta: 'data bus', valor: true },
+      { resposta: 'control bus', valor: false },
+      { resposta: 'id bus', valor: false },
+      { resposta: 'bussines bus', valor: false }
+    ]
+  },
+  {
+    titulo: 'Qual é o processador é considerado custo-benefício',
+    alternativas: [
+      { resposta: 'I9', valor: false },
+      { resposta: 'I5', valor: true },
+      { resposta: 'I7', valor: false },
+      { resposta: 'I10', valor: false }
+    ]
+  },
+  {
+    titulo: 'Qual é o processador em tarefas rápidas e pesadas',
+    alternativas: [
+      { resposta: 'I3', valor: false },
+      { resposta: 'I5', valor: false },
+      { resposta: 'I7', valor: true },
+      { resposta: 'I Celeron', valor: false }
+    ]
+  },
+  {
+    titulo: 'Dois núcleos',
+    alternativas: [
+      { resposta: 'Dual Core', valor: true },
+      { resposta: 'Octal Core', valor: false },
+      { resposta: 'Quad-Core', valor: false },
+      { resposta: 'Hexa-Core', valor: false }
+    ]
+  },
+  {
+    titulo: 'Quatro núcleos',
+    alternativas: [
+      { resposta: 'Deca-Core', valor: false },
+      { resposta: 'Hexa-Core', valor: false },
+      { resposta: 'Octal Core', valor: false },
+      { resposta: 'Quad-Core', valor: true }
+    ]
+  }
+];
